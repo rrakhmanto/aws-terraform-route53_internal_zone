ed9aadbf3485712c9b88a3d94bbba505acefa18b
Riko Rakhmanto <riko.rakhmanto@rackspace.com>
2019-01-02 10:09:14 +1100

    test apply

--- examples/main.tf
+++ examples/main.tf
@@ -1,14 +1,23 @@
 provider "aws" {
   version = "~> 1.2"
-  region  = "us-west-2"
+  region  = "ap-southeast-2"
 }
 
+terraform {
+  required_version = "0.11.8"
+}
+
+module "vpc" {
+  source   = "git@github.com:rackspace-infrastructure-automation/aws-terraform-vpc_basenetwork//?ref=v0.0.6"
+  vpc_name = "Sample-VPC"
+}
+
+
 module "internal_zone" {
-  source = "git@github.com/rackspace-infrastructure-automation/aws-terraform-route53_internal_zone//?ref=v0.0.1"
+  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-route53_internal_zone?ref=v0.0.2"
+  # source = "git@github.com:rrakhmanto/aws-terraform-route53_internal_zone"
+  target_vpc_id = "${module.vpc.vpc_id}"
+  zone_name     = "testing.local"
 
-  zone_name = "example.com"
-
-  environment = "Development"
-
-  target_vpc_id = "vpc-26dd8d42"
+  environment = "Sample"
 }
